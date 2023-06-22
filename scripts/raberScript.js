962a012a97f7c1fa6cd9b7a7ba319d1b1cf3634b
justinsteinberg <justin.steinberg1@aol.com>
2023-06-21 23:14:59 -0400

    Adding sidebar to mobile view

--- scripts/raberScript.js
+++ scripts/raberScript.js
@@ -1,17 +1,7 @@
-function changeBg(){
-	
-	const images = [
-		'url("images/street.jpeg")',
-		'url("images/nyc-cityscape-1.jpeg")',
-		'url("images/nyc-cityscape-2.jpeg")',
-		'url("images/nyc-cityscape-3.jpeg")'
-	];
-
-	const section = document.querySelector(".main");
-	const prev = Math.floor(Math.random() * images.length);
-	const bg = images[prev];
-	section.style.backgroundImage = bg;
-
+function openNav(){
+	document.getElementById("mySideNav").style.width = "200px";
 }
-	
-setInterval(changeBg, 5000);
+
+function closeNav(){
+	document.getElementById("mySideNav").style.width = "0";
+}
