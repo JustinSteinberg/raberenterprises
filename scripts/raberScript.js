ed7ae0c704199ec3ca728cff6dcad5c6b3cc36c0
justinsteinberg <justin.steinberg1@aol.com>
2023-06-28 13:34:58 -0400

    Adding Intersection Oberserver Functionality, along with a bunch of changes to navbar properties when intersection with layer element is less than 85%. Also making sure that the side bar is always visible over others layers even when Intersection Observer is active.

--- scripts/raberScript.js
+++ scripts/raberScript.js
@@ -7,9 +7,9 @@ function closeNav(){
 }
 
 
-const nav = document.getElementsByClassName("header-row");
+/*const nav = document.getElementsByClassName("header-row");
 
-/*window.onscroll = function() {
+window.onscroll = function() {
 	if (document.body.scrollTop >= 50){
 		nav.classList.add("header-show");
 	}
@@ -19,12 +19,53 @@ const nav = document.getElementsByClassName("header-row");
 };
 */
 
-function navCallback(entries){
-	console.log(entries)
+const nav = document.querySelector("nav");
+const layer = document.querySelector(".layer");
+const logo = document.querySelector(".logo-image");
+const img = document.querySelector(".resize");
+const header = document.querySelector("#bus-header");
+const nav_row = document.querySelectorAll(".nav-row")
+const social_media = document.querySelector("#social-media")
+var width = window.innerWidth;
+
+window.onresize = function() {
+	width = window.innerWidth;
+};
+
+const navOptions = {
+	root: null,
+	rootMargin: "-60px 0px 1000px 0px",
+	threshold: 0.90
+};
+
+function navCallback(entries, observer){
+	entries.forEach(entry => {
+		if(!entry.isIntersecting){
+			console.log("False");
+			nav.classList.add("nav-scroll-class");
+			header.classList.add("header-style-scroll");
+			logo.classList.add("logo-image-scroll");
+			img.classList.add("resize-scroll")
+			nav_row[0].style.marginTop ="0px";
+			nav_row[1].style.marginTop ="0px";
+			social_media.style.ho
+		}
+		else{
+			console.log("True");
+			nav.classList.remove("nav-scroll-class");
+			header.classList.remove("header-style-scroll")
+			logo.classList.remove("logo-image-scroll");
+			img.classList.remove("resize-scroll")
+			nav_row[0].style.marginTop ="10px";
+			nav_row[1].style.marginTop ="10px";
+
+
+		}
+	});	
 }
 
-const navOptions = {};
+const navObs = new IntersectionObserver(navCallback, navOptions);
 
-const navObs = new IntersectionObserver(navCallback, navOptions)
+navObs.observe(layer);
 
-navObs.observe(document.querySelector('header'));
+
